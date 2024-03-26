2a2fd685a170f468210b565f8302221e60bd36f2
ArianDJ <148903958+ArianDJ@users.noreply.github.com>
2024-03-26 17:07:41 +0100

    ❗GRAND UPDATE 42: JavaScript VERSION NOW HAS A GUI!

--- JavaScript version/main.js
+++ JavaScript version/main.js
@@ -1,5 +1,572 @@
-var newFont = "Comic Sans MS";
-var e = document.querySelectorAll('*');
-e.forEach(function(t) {
-    t.style.fontFamily = newFont;
+var fontList = `
+Arial
+Helvetica
+Times New Roman
+Courier New
+Verdana
+Georgia
+Comic Sans MS
+Trebuchet MS
+Impact
+Lucida Console
+Lucida Sans Unicode
+Palatino Linotype
+Tahoma
+Geneva
+Lucida Grande
+Book Antiqua
+Candara
+Garamond
+Century Gothic
+Bookman Old Style
+Lucida Bright
+Franklin Gothic Medium
+Arial Rounded MT Bold
+Arial Unicode MS
+Bell MT
+Constantia
+Gill Sans
+Corbel
+Cambria
+Myriad Pro
+MS Reference Sans Serif
+MS Reference Specialty
+MS Reference Serif
+MS Sans Serif
+MS Serif
+Symbol
+Wingdings
+Webdings
+Marlett
+Courier
+Andale Mono
+Bradley Hand ITC
+Brush Script MT
+Vivaldi
+Gigi
+Kristen ITC
+Jokerman
+Wide Latin
+Elephant
+Forte
+Rockwell
+Harlow Solid Italic
+Papyrus
+Parchment
+Algerian
+Bauhaus 93
+Berlin Sans FB
+Blackadder ITC
+Bodoni MT
+Britannic Bold
+Californian FB
+Castellar
+Chiller
+Colonna MT
+Copperplate Gothic Light
+Curlz MT
+Edwardian Script ITC
+Engravers MT
+Freestyle Script
+French Script MT
+Gabriola
+Goudy Old Style
+Harrington
+Imprint MT Shadow
+Informal Roman
+Juice ITC
+Kunstler Script
+Magneto
+Maiandra GD
+Matura MT Script Capitals
+Mistral
+Modern No. 20
+Monotype Corsiva
+Niagara Engraved
+Niagara Solid
+OCR A Extended
+Old English Text MT
+Onyx
+Palace Script MT
+Perpetua
+Playbill
+Ravie
+Showcard Gothic
+Stencil
+Tempus Sans ITC
+Tw Cen MT
+Vladimir Script
+Arial Nova
+Helvetica Neue
+Times Nova
+Univers
+Batang
+BatangChe
+Dotum
+Gulim
+GulimChe
+Malgun Gothic
+Meiryo
+Meiryo UI
+Microsoft Himalaya
+Microsoft JhengHei
+Microsoft YaHei
+MingLiU
+MingLiU-ExtB
+MingLiU_HKSCS
+MingLiU_HKSCS-ExtB
+NSimSun
+PMingLiU
+PMingLiU-ExtB
+SimSun
+SimSun-ExtB
+Arial Black CE
+Arial CE
+Arial Nova Cond
+Arial Rounded MT Bold CE
+Arial Unicode MS CE
+Comic Sans MS CE
+Courier New CE Bold
+Courier New CE Bold Italic
+Courier New CE Italic
+Courier Nova CE
+Helvetica CE Bold
+Helvetica CE Bold Italic
+Helvetica CE Italic
+Helvetica Neue CE 55 Roman
+Helvetica Neue CE 56 Italic
+Helvetica Neue CE 65 Medium
+Helvetica Neue CE 66 Medium Italic
+Helvetica Neue CE 75 Bold
+Helvetica Neue CE 76 Bold Italic
+Helvetica Neue CE 85 Heavy
+Helvetica Neue CE 86 Heavy Italic
+Helvetica Neue CE 95 Black
+Helvetica Neue CE 96 Black Italic
+Malgun Gothic CE
+Meiryo CE
+Meiryo UI CE
+Microsoft Himalaya CE
+Microsoft JhengHei CE
+Microsoft YaHei CE
+MingLiU CE
+MingLiU-ExtB CE
+MingLiU_HKSCS CE
+MingLiU_HKSCS-ExtB CE
+NanumGothic
+NanumGothic Bold
+NanumGothic ExtraBold
+NanumGothic Light
+NanumMyeongjo
+NanumMyeongjo Bold
+NanumMyeongjo ExtraBold
+NanumMyeongjo Light
+NanumSquare
+NanumSquare Bold
+NanumSquare ExtraBold
+NanumSquare Light
+Rod
+Segoe Print CE
+Segoe Script CE
+Segoe UI CE
+Segoe UI Emoji
+Segoe UI Historic
+Segoe UI Symbol
+Yu Gothic
+Yu Gothic UI
+Yu Gothic UI Light
+Yu Gothic UI Semibold
+Yu Gothic UI Semilight
+Yu Gothic UI Bold
+Yu Gothic UI Bold Semilight
+Consolas CE
+Corbel CE
+Corbel Bold CE
+Corbel Bold Italic CE
+Corbel Italic CE
+Segoe UI CE
+Segoe UI Light CE
+Segoe UI Semibold CE
+Segoe UI Semilight CE
+Segoe UI Bold CE
+Segoe UI Bold Italic CE
+Webdings CE
+Wingdings CE
+Wingdings 2 CE
+Wingdings 3 CE
+Abadi MT Condensed Light
+Albertus Extra Bold
+Albertus Medium
+Antique Olive
+Bazooka
+Book Antiqua
+Boulder
+Calisto MT
+Calligrapher
+Century Schoolbook
+Cezanne
+CG Omega
+CG Times
+Charlesworth
+Chaucer
+Clarendon Condensed
+Copperplate Gothic Bold
+Cornerstone
+Coronet
+Cuckoo
+Dauphin
+Denmark
+Fransiscan
+Haettenschweiler
+Heather
+Herald
+Jester
+Letter Gothic
+Lithograph
+Lithograph Light
+Long Island
+Lucida Handwriting
+Lucida Sans
+Marigold
+Market
+Matisse ITC
+MS LineDraw
+News GothicMT
+Old Century
+Pegasus
+Pickwick
+Poster
+Pythagoras
+Sceptre
+Sherwood
+Signboard
+Socket
+Steamer
+Storybook
+Subway
+Technical
+Teletype
+Times
+Unicorn
+Univers Condensed
+Vagabond
+Avenir
+Futura
+Baskerville
+Optima
+Didot
+Bodoni
+Archer
+Caslon
+Avant Garde
+Franklin Gothic
+Gotham
+Myriad
+Minion
+Univers
+Frutiger
+Rockwell
+Trajan
+Helvetica Neue
+Gill Sans
+Clarendon
+FF Meta
+Proxima Nova
+DIN
+Trade Gothic
+Eurostile
+Interstate
+FF Din
+Garamond Premier Pro
+Neutraface
+Avenir Next
+Sabon
+Museo
+Meta
+Optima Nova
+ITC Lubalin Graph
+Mrs Eaves
+Knockout
+Futura PT
+Tungsten
+Gibson
+Avenir Next Condensed
+Avenir Next Rounded
+Brandon Grotesque
+FF Blur
+Gotham Narrow
+Minion Pro
+Warnock Pro
+Courier Prime
+FF Scala
+FF Tisa
+Archer Pro
+Proxima Nova Soft
+Adelle
+Apercu
+Avenir Next Rounded
+Belizio
+Bodoni Egyptian Pro
+Brandon Text
+Chaparral Pro
+Chronicle Display
+Clarendon Text
+Cutive Mono
+Distil
+Epic
+Eskapade
+Estilo
+Fedra Serif A
+Forza
+Freight Big
+Freight Micro
+Freight Neo
+Graphik
+GT America
+Guardian
+Guthen Bloots
+Harriet
+Insignia
+ITC New Baskerville
+Jubilat
+Leitura
+LFT Etica
+Lyon
+Maple
+Mason
+Miller
+Naivety
+National
+Nexus
+Ogg
+Omnes
+Output Sans
+Portrait
+Prumo
+Proxima Nova
+Publico
+Quiroga Serif
+Radnika
+Relato Sans
+Replica
+Revisal
+Riwayat
+Rokkitt
+Rooney Sans
+Roslindale
+Ruda
+Sansita
+Scandia
+Sequel
+Seravek
+Serifa
+Sofia Pro
+Spatha Sans
+Steagal
+Stroudley
+Surt
+Syne
+Tabac Sans
+Tabardo
+Tauran
+Tazugane Info
+TeX Gyre Cursor
+Tienne
+Times Modern
+Titillium Web
+Trirong
+Tundra
+Turnip
+Ubuntu Mono
+Umba Slab
+Uni Neue
+Urban Grotesk
+Vesper Libre
+Vidaloka
+Viga
+Vollkorn
+Work Sans
+XITS
+XCharter
+Zilla Slab
+Poppins
+Lato
+Montserrat
+Open Sans
+Roboto
+Raleway
+Merriweather
+Oswald
+Source Sans Pro
+Nunito
+Ubuntu
+Playfair Display
+Pacifico
+Bitter
+Arimo
+Cabin
+PT Sans
+PT Serif
+Quicksand
+Fira Sans
+Amatic SC
+Josefin Sans
+Yanone Kaffeesatz
+Lobster
+Comfortaa
+Dosis
+Exo
+Karla
+Noto Sans
+Oxygen
+Lora
+Rubik
+Indie Flower
+Cabin Sketch
+Varela Round
+Abril Fatface
+Pinyon Script
+Archivo Black
+Satisfy
+Arvo
+Anton
+Cormorant
+Fjalla One
+Muli
+Inconsolata
+Rokkitt
+Cinzel
+Old Standard TT
+Sacramento
+Permanent Marker
+Concert One
+Pathway Gothic One
+Tangerine
+Lobster Two
+Alegreya
+Signika
+Varela
+Montserrat Alternates
+Amiri
+Nunito Sans
+Work Sans
+`;
+
+var fontNames = fontList.trim().split('\n');
+
+var fontFamilySelect = document.createElement("select");
+fontFamilySelect.id = "font-family";
+
+fontNames.forEach(function(fontName) {
+    var option = document.createElement("option");
+    option.value = fontName.trim(); 
+    option.text = fontName.trim(); 
+    fontFamilySelect.appendChild(option);
 });
+
+var fontChangerContainer = document.createElement("div");
+fontChangerContainer.innerHTML = `
+    <style>
+        .font-changer-container {
+            position: fixed;
+            top: 50%;
+            right: 0;
+            transform: translateY(-50%);
+            background-color: rgba(0, 0, 0, 0.8);
+            color: #fff;
+            padding: 10px;
+            border-radius: 5px;
+            z-index: 9999;
+            display: none;
+        }
+        .font-changer-container select, .font-changer-container input {
+            margin-bottom: 5px;
+            background-color: #333;
+            color: #fff;
+        }
+        .font-changer-container select {
+            width: 100%;
+        }
+        .font-changer-container #sample-text {
+            font-size: 16px;
+            margin-top: 10px;
+        }
+        .font-changer-container button {
+            background-color: #555;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        .font-changer-container button:hover {
+            background-color: #777;
+        }
+        .toggle-button {
+            position: fixed;
+            bottom: 20px;
+            left: 20px; /* Adjust this value */
+            background-color: #555;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            z-index: 9999;
+        }
+        .toggle-button:hover {
+            background-color: #777;
+        }
+    </style>
+    <div class="font-changer-container">
+        <h1 style="margin: 0;">Font Changer</h1>
+        <div>
+            <label for="font-family" style="margin-top: 10px;">Select Font Family:</label>
+        </div>
+        <div>
+            <label for="font-family">Select Font Family:</label>
+        </div>
+        <div>
+            <label for="font-size">Select Font Size:</label>
+            <input type="range" id="font-size" min="8" max="48" step="1" value="16">
+        </div>
+        <div>
+            <button id="apply-font-button">Apply Font</button>
+        </div>
+        <div id="sample-text">
+            Sample Text
+        </div>
+    </div>`;
+
+fontChangerContainer.querySelector(".font-changer-container").appendChild(fontFamilySelect);
+
+document.body.appendChild(fontChangerContainer);
+
+var toggleButton = document.createElement("button");
+toggleButton.innerHTML = "Open Font Changer";
+toggleButton.classList.add("toggle-button");
+document.body.appendChild(toggleButton);
+
+toggleButton.addEventListener("click", function() {
+    var fontChanger = document.querySelector(".font-changer-container");
+    if (fontChanger.style.display === "none") {
+        fontChanger.style.display = "block";
+    } else {
+        fontChanger.style.display = "none";
+    }
+});
+
+document.getElementById("apply-font-button").addEventListener("click", changeFont);
+
+function changeFont() {
+    var fontFamily = document.getElementById("font-family").value;
+    var fontSize = document.getElementById("font-size").value + "px";
+    var sampleText = document.getElementById("sample-text");
+    sampleText.style.fontFamily = fontFamily;
+    sampleText.style.fontSize = fontSize;
+
+    var e = document.querySelectorAll('*');
+    e.forEach(function(t) {
+        t.style.fontFamily = fontFamily;
+    });
+}
